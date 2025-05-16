3a3764e703ae93ca4fd4fa1422c9f55c1d913677
Ruan Pablo <ruanpablocutrimramon@gmail.com>
2025-05-16 21:52:06 +0000

    aula 05 part 1

---
+++ POO/aula03/src/Animal.java
@@ -0,0 +1,39 @@
+package src;
+public class Animal {
+    private String nome;
+    private int idade;
+    private double peso;
+    private String cor;
+
+    public Animal(String nome, int idade, double peso, String cor) {
+        this.nome = nome;
+        this.idade = idade;
+        this.peso = peso;
+        this.cor = cor;
+    }
+    public String getNome() {
+        return nome;
+    }
+    public void setNome(String nome) {
+        this.nome = nome;
+    }
+    public int getIdade() {
+        return idade;
+    }
+    public void setIdade(int idade) {
+        this.idade = idade;
+    }
+    public double getPeso() {
+        return peso;
+    }
+    public void setPeso(double peso) {
+        this.peso = peso;
+    }
+    public String getCor() {
+        return cor;
+    }
+    public void setCor(String cor) {
+        this.cor = cor;
+    }
+    
+}
---
+++ POO/aula03/src/Program.java
@@ -0,0 +1,8 @@
+package src;
+public class Program {
+    public static void main(String[] args) {
+        Leao leao = new Leao("Simba", 5, 190.5, "Amarelo");
+        System.out.println("Nome: " + leao.getNome());
+        leao.rugir();
+    }
+}
